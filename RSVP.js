21b49c1eb9168ebbec2f119a848ca9d7ccf0229a
Nawab-AS <59747708+Nawab-AS@users.noreply.github.com>
2025-12-03 21:33:48 -0500

    Rsvp page now has a time delay and a increment animation

--- RSVP.js
+++ RSVP.js
@@ -1,21 +1,38 @@
-let currentRSVPCount = 470;//do with the back end later smh
+let currentRSVPCount = 832;//do with the back end later smh
 let increment = 0;
+let waitTime = 10;//safety incase the if statements fail
 let fullrsvpCount = 1000;//max rsvp count
 
 let percent = 0;
 
-setInterval(() => {
+//individual speeds so the user doesnt have to wait!
+if(currentRSVPCount > 200){
+    waitTime = 12;
+}if(currentRSVPCount > 400){
+    waitTime = 7;
+}if(currentRSVPCount > 600){
+    waitTime = 3;
+}if (currentRSVPCount > 800){
+    waitTime = 1;
+}
+setTimeout(function() {
+    intervalID = setInterval(updateRSVPBar, waitTime);
+}, 500);
 
-currentRSVPCount = document.getElementById("rsvpRange").value;//get the value from the slider
+function updateRSVPBar() {
+    increment++;
+    if (increment >= currentRSVPCount) {
+        //once completed going to the current rsvp count
+        increment = currentRSVPCount;
+        clearInterval(intervalID);
+    }
+    percent = increment / fullrsvpCount;//current rsvp count over max rsvp count
 
-percent = currentRSVPCount / fullrsvpCount;//current rsvp count over max rsvp count
+    document.getElementById("RSVPbar").style.width = (percent * 100) + "%";//set the bar
 
-document.getElementById("RSVPbar").style.width = (percent * 100) + "%";//set the bar
+    document.getElementById("RSVPbar").innerHTML = 
+    "<span style='width:100%;text-align:center;display:block;'>" + increment +"/"+
+    fullrsvpCount + "</span>";
 
-document.getElementById("RSVPbar").innerHTML = 
-"<span style='width:100%;text-align:center;display:block;'>" + currentRSVPCount +"/"+
- fullrsvpCount + "</span>";
-
-document.getElementById("titleCard").innerText = currentRSVPCount + " RSVPs!";
-
-}, 1000);
+    document.getElementById("titleCard").innerText = increment + " RSVPs!";
+}
